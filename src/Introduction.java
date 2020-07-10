36f0f93ca527162a0bebee8fbf8c7b5e121d58f2
teemj <t.dzhalilbekov@gmail.com>
2020-07-10 22:42:32 +0200

    first fix commit

--- src/Introduction.java
+++ src/Introduction.java
@@ -1,7 +1,7 @@
 public class Introduction {
     public static void main(String[] args) {
 
-            System.out.print(" It's gonna be second fix commit");
+            System.out.print(" It's gonna be first fix commit");
 
     }
 }
