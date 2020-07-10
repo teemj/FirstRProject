11e57b1f5c567e03381491e0d57923c0508787fd
teemj <t.dzhalilbekov@gmail.com>
2020-07-10 21:51:21 +0200

    My first commit

---
+++ src/Introduction.java
@@ -0,0 +1,11 @@
+public class Introduction {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5 ; i++) {
+            System.out.print("My name is Temirlan. ");
+        }
+        System.out.println();
+        for (int i = 0; i < 5; i++) {
+            System.out.println("I want to be a java developer!");
+        }
+    }
+}
