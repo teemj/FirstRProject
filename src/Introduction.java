745673f943275663f686a7938cf25316c8c0cb07
teemj <t.dzhalilbekov@gmail.com>
2020-07-10 22:31:59 +0200

    second fix commit

--- src/Introduction.java
+++ src/Introduction.java
@@ -1,9 +1,7 @@
 public class Introduction {
     public static void main(String[] args) {
-        for (int i = 0; i < 5 ; i++) {
-            System.out.print("My name is Temirlan. ");
-        }
-        System.out.println();
+
+            System.out.print(" It's gonna be second fix commit");
 
     }
 }
